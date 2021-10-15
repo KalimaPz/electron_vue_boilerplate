c7ec429b2eb2a15a3f4ca54d0cce0be62f85f5c2
11SF <nsfolknsf@gmail.com>
2021-10-15 16:28:03 +0700

    finish

---
+++ src/components/TodoListView.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" md="5" class="content">
+      <p v-if="getTodos.length === 0" class="text-center">ไม่มีรายการ</p>
+      <v-list v-else>
+        <v-list-item>
+          <v-list-item-content>
+            <v-row>
+              <v-col cols="1"><p class="text-center font-weight-medium">No.</p></v-col>
+              <v-col cols="9"><p class="text-center font-weight-medium">Title</p></v-col>
+              <v-col cols="2">
+                <p class="text-center font-weight-medium">Done ?</p>
+              </v-col>
+            </v-row>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item v-for="value in getTodos" :key="value.id">
+          <v-list-item-content>
+            <v-row>
+              <v-col cols="1"
+                ><p class="text-center">{{ value.id + 1 }}.</p></v-col
+              >
+              <v-col cols="9"
+                ><p
+                  :class="[
+                    value.done ? 'text-decoration-line-through' : '',
+                    'pl-8',
+                  ]"
+                >
+                  {{ value.title }}
+                </p></v-col
+              >
+              <v-col cols="2">
+                <v-checkbox
+                  class="pa-0 ma-0 pl-9"
+                  v-model="value.done"
+                  color="indigo"
+                  hide-details
+                ></v-checkbox
+              ></v-col>
+            </v-row>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "TodoListView",
+  computed: { ...mapGetters(["getTodos"]) },
+};
+</script>
+
+<style scoped>
+.content {
+  margin: 8rem;
+}
+.card {
+  height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    id: 0,
+    todos: [],
   },
   mutations: {
+    ADDTODO(state, title) {
+      state.todos.push({
+        id: state.id++,
+        title,
+        done: false,
+      });
+    },
+    DELETETODO(state, id) {
+      state.todos.filter(value => value.id !== id);
+    },
+    CHANGESTATUS(state, id) {
+      state.todos[id].done = !state.todos[id].done
+    }
   },
   actions: {
+    addTodo(context, title) {
+      if(!title) {
+        title = `to do #${this.state.id + 1}`
+      }
+      context.commit("ADDTODO", title);
+    },
+    deleteTodo(context, id) {
+      context.commit("DELETETODO", id)
+    },
+    changeStatus(context, id) {
+      context.commit("CHANGESTATUS", id)
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+  getters: {
+    getTodos(state) {
+      return state.todos;
+    },
+  },
+});
